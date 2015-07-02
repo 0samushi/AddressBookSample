b5858f76af7f5f204c1339abb2dca6d1d2da2356
Osamushi <mad.naname@gmail.com>
2015-07-02 15:24:34 +0900

    Initial Commit

---
+++ AddressBookSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AddressBookSample
+//
+//  Created by 加藤直人 on 7/2/15.
+//  Copyright (c) 2015 加藤直人. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
